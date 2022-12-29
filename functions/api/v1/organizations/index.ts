b76c608ca5c72fbcf57363d871491a9100bf0559
miki-matsumoto <mikimatsumoto829@gmail.com>
2022-12-29 15:41:22 +0900

    zod validation

--- functions/api/v1/organizations/index.ts
+++ functions/api/v1/organizations/index.ts
@@ -1,3 +1,24 @@
-export const onRequestPost: PagesFunction = async () => {
-  return new Response("post");
+import { z } from "zod";
+
+const postInput = z.object({
+  org_id: z.string().min(1),
+  name: z.string().optional(),
+});
+
+export const onRequestPost: PagesFunction = async ({ request }) => {
+  const json = await request.json();
+  const parsed = postInput.safeParse(json);
+
+  if (!parsed.success)
+    return new Response(
+      JSON.stringify({
+        message: "Bad Request",
+        issues: JSON.parse(parsed.error.message),
+      }),
+      { status: 400, headers: { "content-type": "application/json" } }
+    );
+
+  return new Response(JSON.stringify(parsed.data), {
+    headers: { "content-type": "application/json" },
+  });
 };
