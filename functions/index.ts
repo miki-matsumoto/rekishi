1fe3d2e8e74c9bc70349d73681ccb1a5f135085e
miki-matsumoto <mikimatsumoto829@gmail.com>
2022-12-31 14:08:21 +0900

    JWT validation

--- functions/index.ts
+++ functions/index.ts
@@ -1,8 +1,38 @@
-export const onRequestGet: PagesFunction = async ({ next, request }) => {
+import { database } from "src/lib/db";
+import { Env } from "src/lib/env";
+import jwt from "@tsndr/cloudflare-worker-jwt";
+
+export const onRequestGet: PagesFunction<Env> = async ({
+  next,
+  request,
+  env,
+}) => {
   const url = new URL(request.url);
   const notFoundUrl = `${url.origin}/not-found`;
   const searchParams = new URLSearchParams(url.search);
 
-  if (!searchParams.has("token")) return Response.redirect(notFoundUrl);
-  return await next();
+  const token = searchParams.get("token");
+  if (!token) return Response.redirect(notFoundUrl);
+
+  const isValid = await jwt.verify(token, "secret");
+  if (!isValid) return Response.redirect(notFoundUrl);
+
+  const { payload } = jwt.decode(token);
+  const db = database(env.DB);
+
+  const organization = await db
+    .selectFrom("organizations")
+    .select("id")
+    .where("org_id", "=", payload.organization)
+    .executeTakeFirst();
+
+  if (!organization) return Response.redirect(notFoundUrl);
+
+  return new Response(null, {
+    status: 301,
+    headers: {
+      Location: `${url.origin}/audit-logs`,
+      "Set-Cookie": "dummy=dummy",
+    },
+  });
 };
