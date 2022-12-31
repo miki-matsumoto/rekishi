cf5462713958799cf23bcfcf437aebf6f172574c
miki-matsumoto <mikimatsumoto829@gmail.com>
2022-12-31 12:31:30 +0900

    WIP: generate portal link

--- functions/api/v1/portal/generate_link/index.ts
+++ functions/api/v1/portal/generate_link/index.ts
@@ -1,3 +1,50 @@
-export const onRequestPost: PagesFunction = () => {
-  return Response.redirect("https://google.com");
+import { z } from "zod";
+import { database } from "src/lib/db";
+import { Env } from "src/lib/env";
+import jwt from "@tsndr/cloudflare-worker-jwt";
+
+const postValidation = z.object({
+  organization: z.string().min(1),
+});
+
+export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
+  // If empty request body, await request.json() throw "Unexpected token in JSON"
+  const json = await request.text().then((res) => (res ? JSON.parse(res) : {}));
+  // const json = await request.json();
+  const parsed = postValidation.safeParse(json);
+
+  if (!parsed.success)
+    return new Response(
+      JSON.stringify({
+        message: "Bad Request",
+        issues: JSON.parse(parsed.error.message),
+      }),
+      { status: 400, headers: { "content-type": "application/json" } }
+    );
+
+  const { organization } = parsed.data;
+  const db = database(env.DB);
+  const org = await db
+    .selectFrom("organizations")
+    .where("org_id", "=", parsed.data.organization)
+    .select("id")
+    .executeTakeFirst();
+
+  if (!org)
+    return new Response(
+      JSON.stringify({ messsage: "Organization not found" }),
+      { headers: { "content-type": "application/json" } }
+    );
+
+  const token = await jwt.sign({ organization }, "secret");
+
+  const requestUrl = new URL(request.url);
+  return new Response(
+    JSON.stringify({
+      url: `${requestUrl.origin}?token=${token}`,
+    }),
+    {
+      headers: { "content-type": "application/json" },
+    }
+  );
 };
