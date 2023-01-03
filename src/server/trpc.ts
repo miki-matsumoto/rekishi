88d08fad23ce3d15682870f33df2c7b72623176b
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-01-03 23:45:10 +0900

    error formatter

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,5 +1,26 @@
 import { initTRPC } from "@trpc/server";
 import { Context } from "src/server/context";
 import superjson from "superjson";
+import { ZodError } from "zod";
 
-export const t = initTRPC.context<Context>().create({ transformer: superjson });
+export const t = initTRPC.context<Context>().create({
+  /**
+   * @see https://trpc.io/docs/v10/data-transformers
+   */
+  transformer: superjson,
+  /**
+   * @see https://trpc.io/docs/v10/error-formatting
+   */
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zod:
+          error.cause instanceof ZodError
+            ? error.cause.flatten().fieldErrors
+            : null,
+      },
+    };
+  },
+});
