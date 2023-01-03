0cc89dc51a66d04b9351eb1f7378dae353552905
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-01-03 23:44:05 +0900

    install superjson

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,4 +1,5 @@
 import { initTRPC } from "@trpc/server";
 import { Context } from "src/server/context";
+import superjson from "superjson";
 
-export const t = initTRPC.context<Context>().create();
+export const t = initTRPC.context<Context>().create({ transformer: superjson });
