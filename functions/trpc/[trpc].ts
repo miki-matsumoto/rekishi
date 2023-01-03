33dddddcf52a8b64ecfc561ba45e445824d56ba6
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-01-03 23:27:24 +0900

    WIP

--- functions/trpc/[trpc].ts
+++ functions/trpc/[trpc].ts
@@ -2,32 +2,36 @@ import { initTRPC } from "@trpc/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { inferAsyncReturnType } from "@trpc/server";
 import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+import { Env } from "src/lib/env";
+import { database } from "src/lib/db";
 
-export function createContext({ req }: FetchCreateContextFnOptions) {
-  console.log(req);
-  return { req };
+export async function createContext({
+  req,
+  env,
+}: FetchCreateContextFnOptions & { env: Env }) {
+  const db = database(env.DB);
+  return { req, db };
 }
 export type Context = inferAsyncReturnType<typeof createContext>;
 
 const t = initTRPC.context<Context>().create();
 const appRouter = t.router({
   hello: t.procedure.query(async ({ ctx }) => {
-    return { hello: "world" };
+    const d = await ctx.db.selectFrom("users").select("id").execute();
+    return { hello: "world", d };
   }),
 });
 
 export type AppRouter = typeof appRouter;
 
-export const onRequest: PagesFunction = async (context): Promise<Response> => {
-  console.log(context.env);
-  return fetchRequestHandler({
+export const onRequest: PagesFunction<Env> = async (context) =>
+  fetchRequestHandler({
     endpoint: "/trpc",
     req: context.request,
     router: appRouter,
     createContext: () =>
       createContext({
         req: context.request,
-        ...context.env,
+        env: context.env,
       }),
   });
-};
