d938a411a0e8b00fa988ca0d1bba0c831ee09bd2
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-02-03 16:35:51 +0900

    WIP

--- functions/api/v1/portal/generate_link/index.ts
+++ functions/api/v1/portal/generate_link/index.ts
@@ -1,56 +1,43 @@
 import { z } from "zod";
-import { database } from "src/lib/db";
-import { Env } from "src/lib/env";
 import jwt from "@tsndr/cloudflare-worker-jwt";
 import { nanoid } from "nanoid";
 import { jsonResponse } from "src/lib/response";
+import { withValidation } from "src/lib/api-middleware/withValidation";
 
 const postValidation = z.object({
   organization: z.string().min(1),
 });
 
-export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
-  // If empty request body, await request.json() throw "Unexpected token in JSON"
-  const json = await request.text().then((res) => (res ? JSON.parse(res) : {}));
-  // const json = await request.json();
-  const parsed = postValidation.safeParse(json);
+export const onRequestPost = withValidation(
+  postValidation,
+  async ({ request, data, env }) => {
+    const { organization } = data.body;
+    const org = await data.db
+      .selectFrom("organizations")
+      .where("organization_id", "=", organization)
+      .select("id")
+      .executeTakeFirst();
 
-  if (!parsed.success)
-    return new Response(
-      JSON.stringify({
-        message: "Bad Request",
-        issues: JSON.parse(parsed.error.message),
-      }),
-      { status: 400, headers: { "content-type": "application/json" } }
-    );
+    if (!org)
+      return new Response(
+        JSON.stringify({ messsage: `Organization: ${organization} not found` }),
+        { headers: { "content-type": "application/json" } }
+      );
 
-  const { organization } = parsed.data;
-  const db = database(env.DB);
-  const org = await db
-    .selectFrom("organizations")
-    .where("organization_id", "=", parsed.data.organization)
-    .select("id")
-    .executeTakeFirst();
+    const key = `key_${nanoid()}`;
+    const expiryDate = new Date();
+    expiryDate.setMinutes(expiryDate.getMinutes() + 5);
+    const timestamp = expiryDate.getTime() / 1000;
 
-  if (!org)
-    return new Response(
-      JSON.stringify({ messsage: "Organization not found" }),
-      { headers: { "content-type": "application/json" } }
-    );
+    await env.PORTAL_SESSION_EXPIRE_KEY.put(key, organization, {
+      expiration: timestamp,
+    });
 
-  const key = `key_${nanoid()}`;
-  const expiryDate = new Date();
-  expiryDate.setMinutes(expiryDate.getMinutes() + 5);
-  const timestamp = expiryDate.getTime() / 1000;
+    const token = await jwt.sign({ organization, key }, "secret");
 
-  await env.PORTAL_SESSION_EXPIRE_KEY.put(key, organization, {
-    expiration: timestamp,
-  });
-
-  const token = await jwt.sign({ organization, key }, "secret");
-
-  const requestUrl = new URL(request.url);
-  return jsonResponse({
-    url: `${requestUrl.origin}/?token=${token}`,
-  });
-};
+    const requestUrl = new URL(request.url);
+    return jsonResponse({
+      url: `${requestUrl.origin}/?token=${token}`,
+    });
+  }
+);
