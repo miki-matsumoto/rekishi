cdd47488767e56e911e0d43a1cccf0118c55e51b
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-02-05 17:17:41 +0900

    trpc input and output

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,7 +1,11 @@
-import { initTRPC } from "@trpc/server";
+import { inferRouterInputs, inferRouterOutputs, initTRPC } from "@trpc/server";
 import { Context } from "src/server/context";
 import superjson from "superjson";
 import { ZodError } from "zod";
+import { AppRouter } from "src/server/routers";
+
+export type Inputs = inferRouterInputs<AppRouter>;
+export type Outputs = inferRouterOutputs<AppRouter>;
 
 export const t = initTRPC.context<Context>().create({
   /**
