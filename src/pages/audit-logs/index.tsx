5d698b42a9c80ae2170185fccd5a6a14947fd51d
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-02-06 12:44:06 +0900

    Add Button

--- src/pages/audit-logs/index.tsx
+++ src/pages/audit-logs/index.tsx
@@ -1,6 +1,7 @@
-import { Block, Button, Text, List, ListItem } from "@tremor/react";
 import { trpc } from "src/lib/trpc";
 import { Link, useRouter } from "@tanstack/react-router";
+import { Button } from "src/components/ui/button";
+import { ChevronLeft } from "lucide-react";
 
 export const AuditLogsPage = () => {
   const { navigate } = useRouter();
@@ -20,7 +21,6 @@ export const AuditLogsPage = () => {
   return (
     <div>
       <Header />
-      <AuditLogList />
     </div>
   );
 };
@@ -28,69 +28,13 @@ export const AuditLogsPage = () => {
 const Header = () => (
   <div className="border-b-gray-200 border">
     <div className="my-0 mx-auto max-w-5xl h-16 flex justify-between items-center">
-      <Button
-        text="Back to App"
-        color="gray"
-        importance="secondary"
-        size="xs"
-      />
-      <Text>Powerd by Rekishi</Text>
+      <Button variant="outline" size="sm">
+        <ChevronLeft className="mr-2 h-4 w-4" />
+        Back to App
+      </Button>
+      <small className="text-sm font-medium leading-none">
+        Powerd by Rekishi
+      </small>
     </div>
   </div>
 );
-
-const AuditLogList = () => {
-  return (
-    <Block marginTop="mt-8">
-      <div className="my-0 mx-auto max-w-5xl border rounded-md">
-        <List>
-          <div className="bg-slate-50 px-4">
-            <ListItem>
-              <span className="flex-1">city</span>
-              <span className="flex-1">rating</span>
-              <span className="flex-2">rating</span>
-            </ListItem>
-          </div>
-          {cities.map((item) => (
-            <div className="">
-              <Link>
-                <ListItem>
-                  <span className="flex-1 py-2 px-4">{item.city}</span>
-                  <span className="flex-1 py-2 px-4">{item.rating}</span>
-                  <span className="flex-2 py-2 px-4">{item.rating}</span>
-                </ListItem>
-              </Link>
-            </div>
-          ))}
-        </List>
-      </div>
-    </Block>
-  );
-};
-
-const cities = [
-  {
-    city: "Athens",
-    rating: "2 open PR",
-  },
-  {
-    city: "Luzern",
-    rating: "1 open PR",
-  },
-  {
-    city: "Zürich",
-    rating: "0 open PR",
-  },
-  {
-    city: "Vienna",
-    rating: "1 open PR",
-  },
-  {
-    city: "Ermatingen",
-    rating: "0 open PR",
-  },
-  {
-    city: "Lisbon",
-    rating: "0 open PR",
-  },
-];
