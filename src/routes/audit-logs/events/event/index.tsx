6ceb6c7f05bb3dd56276181e8bbbac1fc87c658c
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-02-07 17:00:15 +0900

    update router settings

--- src/routes/audit-logs/events/event/index.tsx
+++ src/routes/audit-logs/events/event/index.tsx
@@ -1,8 +1,9 @@
 import { lazy, Route } from "@tanstack/react-router";
 import { layoutRoute } from "src/routes/layouts";
+import { auditLogsRoute } from "../..";
 
 export const eventRoute = new Route({
-  getParentRoute: () => layoutRoute,
-  path: "audit-logs/events/$event",
+  getParentRoute: () => auditLogsRoute,
+  path: "$event",
   component: lazy(() => import("./Event")),
 });
