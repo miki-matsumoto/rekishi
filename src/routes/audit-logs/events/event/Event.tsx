7060fd5d3616b4cb22189ec5b4fb966489a38d0e
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-02-09 20:57:38 +0900

    WIP

--- src/routes/audit-logs/events/event/Event.tsx
+++ src/routes/audit-logs/events/event/Event.tsx
@@ -1,7 +1,10 @@
 import { Suspense } from "react";
 import { Link } from "@tanstack/react-router";
+import { useParams } from "@tanstack/react-router";
 import { Button } from "src/components/ui/button";
-import { ChevronLeft } from "lucide-react";
+import { Activity, ChevronLeft, Globe, MapPin } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "src/components/ui/avatar";
+import { trpc } from "src/lib/trpc";
 
 export default function EventPage() {
   return (
@@ -16,9 +19,98 @@ export default function EventPage() {
             <ChevronLeft className="mr-1 h-4 w-4" />
             <Link to="/audit-logs/events">Audit Logs</Link>
           </Button>
+          <h4 className="scroll-m-20 text-2xl font-semibold tracking-tight flex items-center">
+            Action Title
+          </h4>
+          <code className="relative rounded bg-slate-100 py-[0.2rem] px-[0.3rem] font-mono text-xs font-normal text-slate-900 dark:bg-slate-800 dark:text-slate-400">
+            Action.Name
+          </code>
         </div>
-        <Suspense fallback={<div>Loading... from suspense</div>}></Suspense>
+        <Suspense fallback={<div>Loading... from suspense</div>}>
+          <EventDetail />
+        </Suspense>
       </div>
     </div>
   );
 }
+
+const EventDetail = () => {
+  const { event } = useParams();
+  const [data] = trpc.findEventById.useSuspenseQuery({
+    eventId: event as string,
+  });
+
+  return (
+    <div className="max-w-5xl mx-auto border sm:rounded-md">
+      <div className="flex">
+        <div className="basis-1/3 border-r p-4">
+          <div className="border-b mb-4 pb-4">
+            <div className="flex items-center">
+              <Avatar>
+                <AvatarImage />
+                <AvatarFallback>
+                  {data.user?.name ? data.user.name[0] : ""}
+                </AvatarFallback>
+              </Avatar>
+              <div className="pl-3">
+                <div className="text-base font-medium">{data.user.name}</div>
+                <div className="font-normal text-gray-500 text-xs">
+                  neil.sims@flowbite.com
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <DetailItem>
+              <div>
+                <div className="flex items-center">
+                  <MapPin className="h-4 w-4 mr-2" />
+                  <p className="font-medium text-sm">Location</p>
+                </div>
+                <p className="text-sm font-normal text-gray-600 ml-6">
+                  {data.context.location ? data.context.location : "-"}
+                </p>
+              </div>
+            </DetailItem>
+
+            <DetailItem>
+              <div>
+                <div className="flex items-center">
+                  <Globe className="h-4 w-4 mr-2" />
+                  <p className="font-medium text-sm">Browser</p>
+                </div>
+                <p className="font-normal ml-6 text-gray-600 text-sm">
+                  {data.context.userAgent ? data.context.userAgent : "-"}
+                </p>
+              </div>
+            </DetailItem>
+
+            <DetailItem>
+              <div>
+                <div className="flex items-center">
+                  <MapPin className="h-4 w-4 mr-2" />
+                  <p className="font-medium text-sm">Location</p>
+                </div>
+                <p className="font-normal ml-6 text-gray-600 text-sm">
+                  {data.context.userAgent}
+                </p>
+              </div>
+            </DetailItem>
+          </div>
+        </div>
+        <div className="flex-auto p-4">hello</div>
+      </div>
+    </div>
+  );
+};
+
+const DetailItem = ({ children }: { children: React.ReactNode }) => (
+  <div className="flex items-center text-gray-900 whitespace-nowrap dark:text-white">
+    {children}
+  </div>
+);
+
+// type Props = { children: React.ReactNode; spacing?: number };
+// const VStack = ({ children, spacing = 1 }: Props) => (
+//   <div className={cn(`flex flex-col gap-[${spacing}]`)}>{children}</div>
+// );
--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,6 +1,8 @@
 import { TRPCError } from "@trpc/server";
+import { sql } from "kysely";
 import { cryptoSession } from "src/lib/session";
 import { t } from "src/server/trpc";
+import { z } from "zod";
 
 const isSession = t.middleware(async ({ ctx, next }) => {
   const session = await cryptoSession(ctx.request);
@@ -41,7 +43,6 @@ export const appRouter = t.router({
       .orderBy("occurred_at", "desc")
       .execute();
 
-    console.log({ events });
     return events.map((event) => ({
       id: event.id,
       occurred_at: event.occurred_at,
@@ -54,6 +55,61 @@ export const appRouter = t.router({
       },
     }));
   }),
+  findEventById: protectedProcedure
+    .input(z.object({ eventId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const event = await ctx.db
+        .selectFrom("audit_logs")
+        .where("audit_logs.id", "=", input.eventId)
+        .innerJoin("users", "users.id", "audit_logs.user_id")
+        .innerJoin("actions", "actions.id", "audit_logs.action_id")
+        .innerJoin("context", "context.id", "audit_logs.context_id")
+        .select([
+          "audit_logs.id",
+          "occurred_at",
+          // user
+          "users.name as userName",
+          // action
+          "actions.name as actionName",
+          "actions.title as actionTitle",
+          // context
+          "context.location",
+          "context.user_agent",
+        ])
+        .executeTakeFirst();
+      if (!event) throw new TRPCError({ code: "NOT_FOUND" });
+
+      const d = await ctx.db
+        .selectFrom(["actions", "targets"])
+        .where("actions.id", "=", "action_wDRNZtVF_oLmhcJov8XRi")
+        .select([
+          "actions.id as id",
+          (eb) =>
+            sql`group_concat(json_object(${sql.literal("id")}, ${eb.ref(
+              "targets.id"
+            )}))`.as("target"),
+        ])
+        .groupBy("actions.id")
+        .executeTakeFirst();
+      console.log(d);
+      console.log(JSON.parse(`[${d?.target}]`));
+
+      return {
+        id: event.id,
+        occurred_at: event.occurred_at,
+        user: {
+          name: event.userName,
+        },
+        action: {
+          title: event.actionTitle,
+          name: event.actionName,
+        },
+        context: {
+          location: event.location,
+          userAgent: event.user_agent,
+        },
+      };
+    }),
 });
 
 export type AppRouter = typeof appRouter;
