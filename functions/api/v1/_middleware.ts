00ff2fbbcd54b24e64329563fc7e04001b074a19
miki-matsumoto <mikimatsumoto829@gmail.com>
2023-02-14 13:34:05 +0900

    Api key validation

--- functions/api/v1/_middleware.ts
+++ functions/api/v1/_middleware.ts
@@ -1,9 +1,34 @@
 import { database } from "src/lib/db";
 import { Env } from "src/lib/env";
 
-export const onRequest: PagesFunction<Env> = async ({ next, data, env }) => {
+function getIdTokenFromReq(request: Request) {
+  const idToken = request.headers.get("authorization");
+  if (!idToken) return null;
+  return idToken?.replace(/^Bearer (.*)/, "$1");
+}
+
+export const onRequest: PagesFunction<Env> = async ({
+  next,
+  data,
+  env,
+  request,
+}) => {
+  const token = getIdTokenFromReq(request);
+  if (env.API_KEY !== token) {
+    return new Response(
+      JSON.stringify({
+        error: {
+          message: "Invalid API Key provided",
+        },
+      }),
+      {
+        headers: { "content-type": "application/json" },
+        status: 401,
+      }
+    );
+  }
+
   const db = database(env.DB);
-  console.log(env.API_KEY);
   data.db = db;
   return await next();
 };
